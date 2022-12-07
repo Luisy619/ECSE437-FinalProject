8685b4a87caae6a8d2fad0dee3212d5fe4eb6d60
Luis <59857306+Luisy619@users.noreply.github.com>
2022-12-06 21:45:09 -0500

    New Coffee Test

---
+++ test/CoffeeTest.java
@@ -0,0 +1,40 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import src.Coffee;
+import src.FlatWhite;
+
+public class CoffeeTest {
+
+    Coffee flatWhite = new FlatWhite();
+
+    @Test                                               
+    @DisplayName("Get Coffee Name")   
+    void testGetCoffeeName() {
+        assertEquals("Flat White", flatWhite.getName(),     
+                "It should properly show the name of the coffee");
+    }
+
+    @Test                                               
+    @DisplayName("Get availble Temperature")   
+    void testGetTemperature() {
+        assertEquals("Hot/Cold", flatWhite.getTemperature(),     
+                "It should properly show the availble temperature options");
+    }
+
+    @Test                                               
+    @DisplayName("Get Ingredients")   
+    void testGetIngredients() {
+        ArrayList<String> testList = new ArrayList<>(List.of("coffee bean", "water", "milk"));
+
+        assertEquals(testList, flatWhite.getIngredients(),
+                "It should properly show the ingredients");  
+    }
+}
